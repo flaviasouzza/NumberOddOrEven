cec1213e8c034aa4c0124674f95b814611f33891
Flávia Souza <88092488+flaviasouzza@users.noreply.github.com>
2022-04-26 17:58:09 -0300

    Add files via upload

---
+++ NumberOddOrEven.java
@@ -0,0 +1,17 @@
+package Logicadeprogramacao;
+
+public class NumberOddOrEven {
+    public static void main (String [] args) {
+        int number = 5;
+
+
+        if (number % 2 == 0)
+            System.out.println("Number is Even");
+
+        else {
+            System.out.println(("Number is Odd"));
+        }
+
+
+    }
+}
